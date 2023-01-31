5b05059afcd3abeacf7fe49443dba4595bbc8b86
Aliya Nawal <aliyanawal2003@gmail.com>
2023-01-31 23:38:58 +0530

    Added my project~

---
+++ experiment.js
@@ -0,0 +1,15 @@
+const monthlyrent=600;
+const yearlyrent=monthlyrent * 12;
+console.log(yearlyrent);
+console.log("hello")
+
+
+const SkyIsBlue=true;
+if (SkyIsBlue)
+{
+    console.log("the sky is blue");
+
+}
+else{
+    console.log("the sky is...noy blue?");
+}
